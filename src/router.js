157a629b585093bd4e9ee1b707731c6f7c1ffe50
Martin Sofroniev <117728421+Martin322s@users.noreply.github.com>
2023-07-26 12:46:32 +0300

    render register page

--- src/router.js
+++ src/router.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
+const authController = require('./controllers/authController');
 
 router.get('/', (req, res) => {
     res.render('index');
 });
+
+router.use('/auth', authController);
 
 module.exports = router;
